b28b4066fcd75279e66807956b3737e24c319580
Jia Le <jle26531@gmail.com>
2025-03-09 15:11:20 +0800

    Second Init

---
+++ android/app/src/main/java/org/eu/jiale0709/oasis/MainActivity.java
@@ -0,0 +1,5 @@
+package org.eu.jiale0709.oasis;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.eu.jiale0709.oasis',
+  appName: 'Oasis',
+  webDir: 'out'
+};
+
+export default config;
