eac1ab3f19e96913be5876c1142167dc779f4d51
Jia Le <jle26531@gmail.com>
2025-03-19 06:44:39 +0800

    half input & completed info

--- pages/app/bmi.jsx
+++ pages/app/bmi.jsx
@@ -1,20 +1,11 @@
 import { useRouter } from "next/router";
 import { useState } from "react";
-import { InfoIcon } from "lucide-react";
+import { HiQuestionMarkCircle } from "react-icons/hi";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import {
-    Drawer,
-    DrawerClose,
-    DrawerContent,
-    DrawerDescription,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerTitle,
-    DrawerTrigger,
-} from "@/components/ui/drawer"
+import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer"
 
 
 export default function Bmi() {
@@ -38,25 +29,43 @@ export default function Bmi() {
                     <div className="inline-block">
                         <Image src={'.././Balance.png'} width={30} height={30} alt="Tools"></Image>
                     </div>
-                    {" "}BMI Calculator
+                    {" "}BMI Calculator                 <Drawer>
+                        <DrawerTrigger><HiQuestionMarkCircle className="h-4 w-4" /></DrawerTrigger>
+                        <DrawerContent>
+                            <DrawerHeader>
+                                <DrawerTitle className={'text-xl'}>What is BMI ?</DrawerTitle>
+                                <DrawerDescription>
+                                    <p className="text-base">
+                                        Body mass index (BMI) is a value derived from the mass (weight) and height of a person. The BMI is defined as the body mass divided by the square of the body height, and is expressed in units of kg/m2, resulting from mass in kilograms (kg) and height in metres (m).
+                                    </p>
+                                    <br />
+                                    <p className="text-base">
+                                        The BMI may be determined first by measuring its components by means of a weighing scale and a stadiometer. The multiplication and division may be carried out directly, by hand or using a calculator.
+                                    </p>
+                                    <i>Resources from: </i><a href="https://en.wikipedia.org/wiki/Body_mass_index" target="_blank"><span className="text-lime-500 font-bold dark:text-lime-300">Wikipedia</span></a>
+                                </DrawerDescription>
+                            </DrawerHeader>
+                            <DrawerFooter>
+                                <DrawerClose>
+                                    <Button className={'duration-350 w-full items-center justify-center rounded-full bg-[#bef264] py-1.5 px-3.5 font-medium tracking-tight text-black transition font-bold'}>
+                                        Understood
+                                    </Button>
+                                </DrawerClose>
+                                <br />
+                            </DrawerFooter>
+                        </DrawerContent>
+                    </Drawer>
                 </h1>
-                <Drawer>
-                    <DrawerTrigger>Open</DrawerTrigger>
-                    <DrawerContent>
-                        <DrawerHeader>
-                            <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-                            <DrawerDescription>This action cannot be undone.</DrawerDescription>
-                        </DrawerHeader>
-                        <DrawerFooter>
-                            <DrawerClose>
-                                <Button className={'duration-350 w-full items-center justify-center rounded-full bg-[#bef264] py-1.5 px-3.5 font-medium tracking-tight text-black transition font-bold'}>
-                                    Understood
-                                </Button>
-                            </DrawerClose>
-                            <br />
-                        </DrawerFooter>
-                    </DrawerContent>
-                </Drawer>
+                <div className="grid w-full max-w-sm items-center gap-1.5">
+                    <Label htmlFor="weight">Weight (Kg) :</Label>
+                    <Input
+                        value={weight}
+                        onChange={(e) => setWeight(e.target.value)}
+                        placeholder="Your weight (kg) ?"
+                        required
+                        id="weight"
+                        variant="secondary"
+                    /></div>
             </div>
         </>
     );
